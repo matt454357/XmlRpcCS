1e7a6c6633795d46b2e22e81f11c10d6a59dffc2
Cinder Biscuits <cinder@cinderblocks.biz>
2017-09-03 15:40:54 -0500

    Update for .NET Core 2.0, dropped Proxy support as .NET Core does not support Remoting at this time

--- XmlRpcCs/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyVersion("2.0.1.0")]
-[assembly: AssemblyTitle("XmlRpcCS")]
-[assembly: AssemblyCompany("Cinderblocks Design Co.")]
-[assembly: AssemblyDescription("Simple small XML-RPC client and server")]
